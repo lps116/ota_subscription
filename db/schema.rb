44506da85ca72923bec84c48c665bc81ea5919a6
Linus Strobel <lps116@ic.ac.uk>
2019-08-02 16:14:25 +0100

    money rails added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_02_134604) do
+ActiveRecord::Schema.define(version: 2019_08_02_150501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_08_02_134604) do
     t.integer "duration"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "price_cents", default: 0, null: false
     t.index ["user_id"], name: "index_plans_on_user_id"
   end
 
