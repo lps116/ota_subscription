f753437a5769f670bb14caa4cb577d62d0af95f9
Linus Strobel <lps116@ic.ac.uk>
2019-08-02 14:53:29 +0100

    model generated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates_presence_of [:first_name, :last_name]
+  has_many :plans, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
