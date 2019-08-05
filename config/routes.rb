21c7ad8e838f2aaee5d46d218e8e7942d7567769
Linus Strobel <lps116@ic.ac.uk>
2019-08-05 09:41:04 +0100

    users page added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :users, only: [:show] do
+  resources :users, only: [:show, :index] do
     resources :plans, only: [:new, :create, :index, :show, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
